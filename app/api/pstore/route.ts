cf431ac5197caea0717dbf150084a209ced7eea7
winstonmeq <winstonmequila@gmail.com>
2025-10-11 20:37:57 +0800

    update api imageurl privateStore

--- app/api/pstore/route.ts
+++ app/api/pstore/route.ts
@@ -25,6 +25,7 @@ export async function GET(request: NextRequest) {
         privateName: true,
         privateDes: true,
         privatePhone: true,
+        pimageurl: true,
         provId: true,
         munId: true,
       },
